5b705a3017559ee071a872f205f5e9f785c2864b
zCrin <antoine.nga@icloud.com>
2016-08-29 18:14:28 +0200

    Update guillermin-hash.api.js

--- guillermin-hash.api.js
+++ guillermin-hash.api.js
@@ -1,16 +1,15 @@
  /* API Js créée par zCrin le 26/09/2016 
 !!! Attention : pensez à ajouter jQuery.
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-!!! Attention : pensez à ajouter le fichier emojii.css joint dans le fichier zip pour l'affichage des emoticons. Vous pouvez aussi directement le charger depuis nos serveurs :
-<link rel="stylesheet" type="text/css" media="all" href="http://epsil0n1.tk/guillermin_hash/download/emoji.css"/> 
+!!! Attention : pensez à ajouter le fichier emojii.css joint dans le fichier zip pour l'affichage des emoticons. 
 Pour hashez une chaîne de caractère : guillermin_hash(string $string,function sucess);
 Pour récupérer la chaîne initiale : guillermin_dehash(string $string,boolean $NewLine,function success);
 $string = la chaîne sur laquelle effectuer la fonction
 $newLine = true ou false: retour la ligne <br /> (false) ou \n(true)
 success = fonction contenant comme paramètres le résultat
 Note : aucune donnée ne sera conservée sur nos serveurs 
-!!! Attention : la version JavaScript de guillermin_hash() est instable. Préférez la version PHP ou vérifiez le résultat de la fonction.
-Télécharger la version PHP de la fonction : http://epsil0n1.tk/download/guillermin_hash.zip*/
+Plus d'informations sur le wiki github : https://github.com/zCrin/guillermin_hash/wiki
+*/
 function guillermin_hash($chaine, callback){
 	$.post( "http://epsil0n1.tk/guillermin_hash/api.php", { hash : $chaine },function(result){
 		result = result.substring(4, (result.length - 1));
