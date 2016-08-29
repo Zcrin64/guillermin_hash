4208d17b8ae2034e4097327d20f02df65ff0375f
zCrin <antoine.nga@icloud.com>
2016-08-29 18:11:56 +0200

    Update guillermin-hash.api.php
    Fixed a bug linked to two followed underscore

--- guillermin-hash.api.php
+++ guillermin-hash.api.php
@@ -1,7 +1,6 @@
 <?php
 /* API PHP créée par zCrin le 26/09/2016
-!!! Attention : pensez à ajouter le fichier emojii.css joint dans le fichier zip pour l'affichage des emoticons. Vous pouvez aussi directement le charger depuis nos serveurs :
-<link rel="stylesheet" type="text/css" media="all" href="http://epsil0n1.tk/guillermin_hash/download/emoji.css"/>
+!!! Attention : pensez à ajouter le fichier emojii.css joint dans le fichier zip pour l'affichage des emoji.
 !!! Attention : Votre page doit être encodé en utf8 et utiliser le charset utf8
 <meta charset="utf-8"/>
 Pour hashez une chaîne de caractère : guillermin_hash(string $string);
@@ -9,16 +8,15 @@ Pour récupérer la chaîne initiale : guillermin_dehash(string $string[,boolean
 $string = la chaîne sur laquelle effectuer la fonction
 $newLine = true ou false: retour la ligne <br /> (false) ou \n(true)
 Note : aucune donnée ne sera conservée sur nos serveurs 
-!!! Attention : la version JavaScript de guillermin_hash() est instable. Préférez la version PHP ou vérifiez le résultat de la fonction.
-Télécharger la version JS de la fonction : http://epsil0n1.tk/download/guillermin_hash.zip*/
+Plus d'informations sur le wiki github https://github.com/zCrin/guillermin_hash/wiki
 function guillermin_hash($chaine){
 	$chaine = file_get_contents('http://epsil0n1.tk/guillermin_hash/api.php?hash='.urlencode($chaine));
-	$chaine = preg_replace('#(  _)(.*)_(.*)#siU','$2',$chaine);
+	$chaine = substr(substr($chaine,0,-1), 3);
 	return $chaine;
 }
 function guillermin_dehash($chaine, $nl = false){
 	$chaine = file_get_contents('http://epsil0n1.tk/guillermin_hash/api.php?dehash='.urlencode($chaine));
-	$chaine = preg_replace('#(  _)(.*)_(.*)#U','$2',$chaine);
+	$chaine = substr(substr($chaine,0,-1), 3);
 	if($nl == true){
 		$chaine = str_replace('<br />',"\n",$chaine);
 	}
